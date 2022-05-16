6dace174281c8d45b2dd692c0f131216dffd68b7
Supuni22 <supunimanamperi@gmail.com>
2022-05-16 12:56:42 +0530

    Created register and login functionalities with bcryptjs

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const { createPool } = require("mysql");
+const bcrypt = require("bcryptjs");
+
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({
+// extended: true
+// }));
 
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
@@ -26,6 +32,41 @@ app.get("/", (req, res)=>{
             res.send(result);
         }
     });
+});
+
+app.post("/register", (req, res)=> {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    pool.query("INSERT INTO users(UserName, Password) values('"+req.body.userName+"', '"+hashedPassword+"')", (err, result, field)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log("User successfully registered");
+            res.send("User successfully registered");
+        }
+    }); 
+});
+
+app.post("/login", (req, res)=> {
+
+    pool.query("SELECT * FROM users WHERE UserName = '"+req.body.userName+"'", (err, result, field)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(result.length > 0){
+                if(bcrypt.compareSync(req.body.password, result[0].Password)){
+                    res.send("User authenticated");
+                }else{
+                    res.send("Incorrect credentials!");
+                }
+            }else{
+                res.send("User can't find");
+            }
+           
+        }
+       
+    }); 
 });
 
 app.listen(PORT, (req, res)=>{
